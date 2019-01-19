29be8761c37eb78e1fbf3b2f7ed3c40d436ba13a
may_fsac <may_fsac@126.com>
2019-01-19 17:03:06 +0800

    Initial commit

---
+++ src/main/java/com/qf/gitdemo/controller/Test.java
@@ -0,0 +1,22 @@
+package com.qf.gitdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author ChenShuaiJian
+ * @Date 2019/1/19
+ */
+@Controller
+@RequestMapping(value = "test")
+public class Test {
+
+    @RequestMapping(value = "add")
+    @ResponseBody
+    public String add(){
+        System.out.println("git");
+
+        return  "ok";
+    }
+}
